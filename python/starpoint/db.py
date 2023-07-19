2f31f11fc5deaee921b9ac9448f9d506a395d0ad
jsungg <jsung@pointable.ai>
2023-07-19 01:31:17 -0700

    updated tests for python starpoint sdk db client + updated returned response for build_and_insert_embeddings_from_open_api

--- python/starpoint/db.py
+++ python/starpoint/db.py
@@ -583,7 +583,10 @@ class Client(object):
         embedding_data = embedding_response.get("data")
         if embedding_data is None:
             LOGGER.warning(NO_EMBEDDING_DATA_FOUND)
-            return embedding_response
+            return {
+                "openai_response": embedding_response,
+                "starpoint_response": None,
+            }
 
         if document_metadatas is None:
             LOGGER.info(
@@ -598,7 +601,7 @@ class Client(object):
         try:
             sorted_embedding_data = sorted(embedding_data, key=lambda x: x["index"])
             embeddings = map(lambda x: x.get("embedding"), sorted_embedding_data)
-            self.column_insert(
+            starpoint_response = self.column_insert(
                 embeddings=embeddings,
                 document_metadatas=document_metadatas,
                 collection_id=collection_id,
@@ -609,5 +612,9 @@ class Client(object):
                 "An exception has occurred while trying to load embeddings into the db. "
                 f"This is the error:\n{e}"
             )
+            starpoint_response = {"error": str(e)}
 
-        return embedding_response
+        return {
+            "openai_response": embedding_response,
+            "starpoint_response": starpoint_response,
+        }
