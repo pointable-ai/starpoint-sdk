bdd10c843883201c166f22178b3da4c031a1f929
Justin Sung <47491122+jsungg@users.noreply.github.com>
2023-07-29 21:52:11 -0700

    update type export in sdk (#26)

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -64,3 +64,9 @@ const initialize = (
 };
 
 export const db = { initialize };
+
+export * from "./writer/types";
+export * from "./embedding/types";
+export * from "./reader/types";
+export * from "./manager/types";
+export * from "./common-types";
