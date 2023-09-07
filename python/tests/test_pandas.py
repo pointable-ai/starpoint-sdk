25f9009956ec0ca450b19bbcfd1e437779f2c581
Zapple <brian@pointable.ai>
2023-09-06 22:53:06 -0500

    Refactor test to test util code

--- python/tests/test_pandas.py
+++ python/tests/test_pandas.py
@@ -8,6 +8,27 @@ from _pytest.monkeypatch import MonkeyPatch
 from starpoint import pandas
 
 
+def test__check_column_lenth_too_many_columns(monkeypatch: MonkeyPatch):
+    """Tests that when there are too many columns a warning is logged, but
+    exception is not raised."""
+    too_many_column_dataframe = pd.DataFrame(
+        [[1, 2, 3]],
+        columns=["embedding", "metadata", "extra"],
+    )
+
+    logger_mock = MagicMock()
+    monkeypatch.setattr(pandas, "LOGGER", logger_mock)
+    pandas._check_column_length(too_many_column_dataframe)
+
+
+@pytest.mark.parametrize("dataframe_columns", [[], [[1]], [[1], [2]]])
+def test__check_column_lenth_too_few_columns(dataframe_columns):
+    too_few_column_dataframe = pd.DataFrame(dataframe_columns)
+
+    with pytest.raises(ValueError, match=pandas.TOO_FEW_COLUMN_ERROR):
+        pandas._check_column_length(too_few_column_dataframe)
+
+
 def test_init_pandas_client():
     mock_startpoint_client = MagicMock()
     pandas_client = pandas.PandasClient(mock_startpoint_client)
@@ -67,14 +88,3 @@ def test_insert_by_dataframe_missing_metadata_column():
         pandas.MISSING_COLUMN.substitute(column_name="metadata")
         in excinfo.value.__notes__
     )
-
-
-@pytest.mark.parametrize("dataframe_columns", [[], [[1]], [[1], [2]]])
-def test_insert_by_dataframe_too_few_columns(dataframe_columns):
-    mock_startpoint_client = MagicMock()
-    pandas_client = pandas.PandasClient(mock_startpoint_client)
-
-    too_few_column_dataframe = pd.DataFrame(dataframe_columns)
-
-    with pytest.raises(ValueError, match=pandas.TOO_FEW_COLUMN_ERROR):
-        pandas_client.insert_by_dataframe(too_few_column_dataframe)
