52b4d6612dfc36e02f7f137e0cd6f5e51f444be7
Zapple <brian@pointable.ai>
2023-09-13 16:39:43 -0500

    Fix embed method and clarify variable names

--- python/starpoint/embedding.py
+++ python/starpoint/embedding.py
@@ -38,7 +38,6 @@ class EmbeddingClient(object):
         self.host = _validate_host(host)
         self.api_key = api_key
 
-    # FIXME: auto-fill the metadata field
     def embed(
         self,
         texts: List[str],
@@ -58,27 +57,8 @@ class EmbeddingClient(object):
         Raises:
             requests.exceptions.SSLError: Failure likely due to network issues.
         """
-        request_data = dict(text=text, model=model.value)
-        try:
-            response = requests.post(
-                url=f"{self.host}{EMBED_PATH}",
-                json=request_data,
-                headers=_build_header(
-                    api_key=self.api_key,
-                    additional_headers={"Content-Type": "application/json"},
-                ),
-            )
-        except requests.exceptions.SSLError as e:
-            LOGGER.error(SSL_ERROR_MSG)
-            raise
-
-        if not response.ok:
-            LOGGER.error(
-                f"Request failed with status code {response.status_code} "
-                f"and the following message:\n{response.text}"
-            )
-            return {}
-        return response.json()
+        text_embedding_items = [{"text": text, "metadata": None} for text in texts]
+        return self.embed_items(text_embedding_items=text_embedding_items, model=model)
 
     def embed_and_join_metadata(
         self,
@@ -92,6 +72,7 @@ class EmbeddingClient(object):
         Args:
             texts: List of strings to create embeddings from.
             metadatas: List of metadata to join to the string and embedding when the embedding operation is complete.
+                This metadata makes your embeddings queryable within starpoint.
             model: An enum choice from EmbeddingModel.
 
         Returns:
@@ -100,25 +81,25 @@ class EmbeddingClient(object):
         Raises:
             requests.exceptions.SSLError: Failure likely due to network issues.
         """
-        items = [
+        text_embedding_items = [
             {
                 "text": text,
                 "metadata": metadata,
             }
             for text, metadata in zip(texts, metadatas)
         ]
-        return self.embed_items(items=items, model=model)
+        return self.embed_items(text_embedding_items=text_embedding_items, model=model)
 
     def embed_items(
         self,
-        items: List[Dict],
+        text_embedding_items: List[Dict],
         model: EmbeddingModel,
     ) -> Dict[str, List[Dict]]:
         """Takes items with text and metadata, and embeds the text using a model in starpoint. Metadata is joined with
         the results for ergonomics.
 
         Args:
-            items: List of dict where the text and metadata are paired together
+            text_embedding_items: List of dict where the text and metadata are paired together
             model: An enum choice from EmbeddingModel.
 
         Returns:
@@ -127,7 +108,7 @@ class EmbeddingClient(object):
         Raises:
             requests.exceptions.SSLError: Failure likely due to network issues.
         """
-        request_data = dict(items=items, model=model.value)
+        request_data = dict(items=text_embedding_items, model=model.value)
         try:
             response = requests.post(
                 url=f"{self.host}{EMBED_PATH}",
