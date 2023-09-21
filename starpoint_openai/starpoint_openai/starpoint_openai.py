ad45e3a6218e16ca2402b75e7ebda68f46d4e5c8
Zapple <brian@pointable.ai>
2023-09-20 19:09:46 -0500

    Add starpoint openai dir

---
+++ starpoint_openai/starpoint_openai/starpoint_openai.py
@@ -0,0 +1,166 @@
+import logging
+from pathlib import Path
+from typing import Any, Dict, Iterable, List, Optional, Union
+
+import openai
+
+from starpoint import db, _utils
+
+LOGGER = logging.getLogger(__name__)
+
+
+NO_API_KEY_VALUE_ERROR = "Please provide at least one value for either api_key or filepath where the api key lives."
+MULTI_API_KEY_VALUE_ERROR = "Please only provide either api_key or filepath with the api_key in your initialization."
+NO_API_KEY_FILE_ERROR = "The provided filepath for the API key is not a valid file."
+
+# OpenAI response error
+NO_EMBEDDING_DATA_FOUND = (
+    "No embedding data found in the embedding response from OpenAI."
+)
+
+DEFAULT_OPENAI_MODEL = "text-embedding-ada-002"
+
+
+class OpenAIClient(object):
+    def __init__(
+        self,
+        starpoint: db.Client,
+        openai_key: Optional[str] = None,
+        openai_key_filepath: Optional[str] = None,
+    ):
+        self.starpoint = starpoint
+        self._init_openai(openai_key, openai_key_filepath)
+
+    def _init_openai(
+        self,
+        openai_key: Optional[str] = None,
+        openai_key_filepath: Optional[str] = None,
+    ):
+        """Initializes OpenAI functionality by setting up the openai client.
+
+        Args:
+            openai_key: OpenAI API key.
+            openai_key_filepath: Filepath that contains an OpenAI API key.
+
+        Raises:
+            ValueError: Both API key and filepath to API key is provided. Only one is allowed.
+            ValueError: No API key or filepath provided.
+            ValueError: API key filepath provided is not a file.
+        """
+        self.openai = openai
+        # TODO: maybe do this for starpoint api_key also
+
+        if openai_key and openai_key_filepath:
+            raise ValueError(MULTI_API_KEY_VALUE_ERROR)
+        elif openai_key is None:
+            if openai_key_filepath is None:
+                raise ValueError(NO_API_KEY_VALUE_ERROR)
+            if not Path(openai_key_filepath).is_file():
+                raise ValueError(NO_API_KEY_FILE_ERROR)
+            self.openai.api_key_path = openai_key_filepath
+        else:
+            self.openai.api_key = openai_key
+
+    def build_and_insert_embeddings(
+        self,
+        model: str,
+        input_data: Union[str, Iterable[Any]],
+        document_metadatas: Optional[List[Dict[Any, Any]]] = None,
+        collection_id: Optional[str] = None,
+        collection_name: Optional[str] = None,
+        openai_user: Optional[str] = None,
+    ) -> Dict[Any, Any]:
+        """Builds and inserts embeddings into starpoint by requesting embedding creation from
+        an initialized openai client. Regardless whether the operation insert data into starpoint is
+        successful, the response from the openai client is returned.
+
+        Args:
+            model: Embedding model to be used for creating the embeddings. For an up to date list of what
+                models are available see [OpenAI's guide on embeddings](https://platform.openai.com/docs/guides/embeddings/what-are-embeddings)
+            input_data: Data to be embedded.
+            document_metadatas: Optional metadatas to tie to the embeddings generated using the input data.
+            collection_id: The collection's id where the documents will be updated.
+                This or the `collection_name` needs to be provided.
+            collection_name: The collection's name where the documents will be updated.
+                This or the `collection_id` needs to be provided.
+            openai_user: Optional user string used by the embedding endpoint from OpenAI.
+
+        Returns:
+            dict: Responses from starpoint and openai.
+        """
+        _utils._check_collection_identifier_collision(collection_id, collection_name)
+
+        embedding_response = self.openai.Embedding.create(
+            model=model, input=input_data, user=openai_user
+        )
+
+        embedding_data = embedding_response.get("data")
+        if embedding_data is None:
+            LOGGER.warning(NO_EMBEDDING_DATA_FOUND)
+            return {
+                "openai_response": embedding_response,
+                "starpoint_response": None,
+            }
+
+        if document_metadatas is None:
+            LOGGER.info(
+                "No custom document_metadatas provided. Using input_data for the document_metadatas"
+            )
+            if isinstance(input_data, str):
+                document_metadatas = [{"input": input_data}]
+            else:
+                document_metadatas = [{"input": data} for data in input_data]
+
+        # Return the embedding response no matter what issues/bugs we might run into in the sdk
+        try:
+            sorted_embedding_data = sorted(embedding_data, key=lambda x: x["index"])
+            embeddings = map(lambda x: x.get("embedding"), sorted_embedding_data)
+            starpoint_response = self.starpoint.column_insert(
+                embeddings=embeddings,
+                document_metadatas=document_metadatas,
+                collection_id=collection_id,
+                collection_name=collection_name,
+            )
+        except Exception as e:
+            LOGGER.error(
+                "An exception has occurred while trying to load embeddings into the db. "
+                f"This is the error:\n{e}"
+            )
+            starpoint_response = {"error": str(e)}
+
+        return {
+            "openai_response": embedding_response,
+            "starpoint_response": starpoint_response,
+        }
+
+    def default_build_and_insert_embeddings(
+        self,
+        input_data: Union[str, Iterable[Any]],
+        collection_id: Optional[str] = None,
+        collection_name: Optional[str] = None,
+    ) -> Dict[Any, Any]:
+        """Builds and inserts embeddings into starpoint by requesting embedding creation from
+        an initialized openai client using the model `text-embedding-ada-002`. If you wish to use other models use the
+        `build_and_insert_embeddings` method instead. Regardless whether the operation into starpoint is successful,
+        the response from the openai client is returned.
+
+        Args:
+            model: Embedding model to be used for creating the embeddings. For an up to date list of what
+                models are available see [OpenAI's guide on embeddings](https://platform.openai.com/docs/guides/embeddings/what-are-embeddings)
+            input_data: Data to be embedded.
+            document_metadatas: Optional metadatas to tie to the embeddings generated using the input data.
+            collection_id: The collection's id where the documents will be updated.
+                This or the `collection_name` needs to be provided.
+            collection_name: The collection's name where the documents will be updated.
+                This or the `collection_id` needs to be provided.
+            openai_user: Optional user string used by the embedding endpoint from OpenAI.
+
+        Returns:
+            dict: Responses from starpoint and openai.
+        """
+        return self.build_and_insert_embeddings(
+            model=DEFAULT_OPENAI_MODEL,
+            input_data=input_data,
+            collection_id=collection_id,
+            collection_name=collection_name,
+        )
---
+++ starpoint_openai/tests/test_openai.py
@@ -0,0 +1,279 @@
+from tempfile import NamedTemporaryFile
+from uuid import uuid4
+from unittest.mock import MagicMock, patch
+
+import pytest
+from _pytest.monkeypatch import MonkeyPatch
+
+from starpoint_openai import starpoint_openai
+
+
+@patch("starpoint.reader.Reader")
+@patch("starpoint.writer.Writer")
+def test_client_init_openai_no_api_value(
+    mock_writer: MagicMock, mock_reader: MagicMock
+):
+    with pytest.raises(ValueError, match=openai.NO_API_KEY_VALUE_ERROR):
+        openai.OpenAIClient(MagicMock())
+
+
+@patch("starpoint.reader.Reader")
+@patch("starpoint.writer.Writer")
+def test_client_init_openai_both_api_value(
+    mock_writer: MagicMock, mock_reader: MagicMock
+):
+    with pytest.raises(ValueError, match=openai.MULTI_API_KEY_VALUE_ERROR):
+        openai.OpenAIClient(MagicMock(), "mock_key", "mock_path")
+
+
+@patch("starpoint.reader.Reader")
+@patch("starpoint.writer.Writer")
+def test_client_init_openai_with_api_key(
+    mock_writer: MagicMock, mock_reader: MagicMock
+):
+    mock_api_key = "mock_key"
+
+    client = openai.OpenAIClient(MagicMock(), openai_key=mock_api_key)
+
+    assert client.openai.api_key == mock_api_key
+
+
+@patch("starpoint.reader.Reader")
+@patch("starpoint.writer.Writer")
+def test_client_init_openai_with_api_path(
+    mock_writer: MagicMock, mock_reader: MagicMock
+):
+    temp_file = NamedTemporaryFile()
+
+    client = openai.OpenAIClient(MagicMock(), openai_key_filepath=temp_file.name)
+
+    assert client.openai.api_key_path == temp_file.name
+
+
+@patch("starpoint.reader.Reader")
+@patch("starpoint.writer.Writer")
+def test_client_init_openai_with_bad_api_path(
+    mock_writer: MagicMock, mock_reader: MagicMock
+):
+    mock_api_key_path = "1234~/path"
+
+    with pytest.raises(ValueError, match=openai.NO_API_KEY_FILE_ERROR):
+        openai.OpenAIClient(MagicMock(), openai_key_filepath=mock_api_key_path)
+
+
+@patch("starpoint.openai._utils._check_collection_identifier_collision")
+@patch("starpoint.openai.OpenAIClient._init_openai")
+@patch("starpoint.reader.Reader")
+@patch("starpoint.writer.Writer")
+def test_client_build_and_insert_embeddings_input_string_success(
+    mock_writer: MagicMock,
+    mock_reader: MagicMock,
+    mock_init_openai: MagicMock,
+    collision_mock: MagicMock,
+):
+    starpoint_mock = MagicMock()
+    client = openai.OpenAIClient(starpoint_mock)
+    openai_mock = MagicMock()
+    client.openai = openai_mock
+
+    mock_embedding = 0.77
+    expected_embedding_response = {
+        "data": [
+            {
+                "embedding": mock_embedding,
+                "index": 0,
+            }
+        ]
+    }
+    openai_mock.Embedding.create.return_value = expected_embedding_response
+
+    mock_input = "mock_input"
+    mock_model = "mock-model"
+
+    expected_build_and_insert_response = {
+        "openai_response": expected_embedding_response,
+        "starpoint_response": starpoint_mock.column_insert(),
+    }
+
+    actual_build_and_insert_response = client.build_and_insert_embeddings(
+        model=mock_model, input_data=mock_input
+    )
+
+    assert actual_build_and_insert_response == expected_build_and_insert_response
+    collision_mock.assert_called_once()
+
+    # independently check args since embeddings is a map() generator and cannot be checked against simple equality
+    insert_call_kwargs = starpoint_mock.column_insert.call_args.kwargs
+    assert [mock_embedding] == list(insert_call_kwargs.get("embeddings"))
+    assert [{"input": mock_input}] == insert_call_kwargs.get("document_metadatas")
+    assert insert_call_kwargs.get("collection_id") is None  # default value
+    assert insert_call_kwargs.get("collection_name") is None  # default value
+
+
+@patch("starpoint.openai._utils._check_collection_identifier_collision")
+@patch("starpoint.openai.OpenAIClient._init_openai")
+@patch("starpoint.reader.Reader")
+@patch("starpoint.writer.Writer")
+def test_client_build_and_insert_embeddings_input_list_success(
+    mock_writer: MagicMock,
+    mock_reader: MagicMock,
+    mock_init_openai: MagicMock,
+    collision_mock: MagicMock,
+):
+    starpoint_mock = MagicMock()
+    client = openai.OpenAIClient(starpoint_mock)
+    openai_mock = MagicMock()
+    client.openai = openai_mock
+
+    expected_embedding_response = {
+        "data": [
+            {
+                "embedding": 0.77,
+                "index": 0,
+            },
+            {
+                "embedding": 0.88,
+                "index": 1,
+            },
+        ]
+    }
+    openai_mock.Embedding.create.return_value = expected_embedding_response
+
+    mock_input = ["mock_input1", "mock_input2"]
+    mock_model = "mock-model"
+
+    expected_build_and_insert_response = {
+        "openai_response": expected_embedding_response,
+        "starpoint_response": starpoint_mock.column_insert(),
+    }
+
+    actual_build_and_insert_response = client.build_and_insert_embeddings(
+        model=mock_model, input_data=mock_input
+    )
+
+    assert actual_build_and_insert_response == expected_build_and_insert_response
+    collision_mock.assert_called_once()
+
+    # independently check args since embeddings is a map() generator and cannot be checked against simple equality
+    insert_call_kwargs = starpoint_mock.column_insert.call_args.kwargs
+    assert [0.77, 0.88] == list(insert_call_kwargs.get("embeddings"))
+    assert [
+        {"input": "mock_input1"},
+        {"input": "mock_input2"},
+    ] == insert_call_kwargs.get("document_metadatas")
+    assert insert_call_kwargs.get("collection_id") is None  # default value
+    assert insert_call_kwargs.get("collection_name") is None  # default value
+
+
+@patch("starpoint.openai._utils._check_collection_identifier_collision")
+@patch("starpoint.openai.OpenAIClient._init_openai")
+@patch("starpoint.reader.Reader")
+@patch("starpoint.writer.Writer")
+def test_client_build_and_insert_embeddings_no_data_in_response(
+    mock_writer: MagicMock,
+    mock_reader: MagicMock,
+    mock_init_openai: MagicMock,
+    collision_mock: MagicMock,
+    monkeypatch: MonkeyPatch,
+):
+    starpoint_mock = MagicMock()
+    client = openai.OpenAIClient(starpoint_mock)
+    openai_mock = MagicMock()
+    client.openai = openai_mock
+
+    expected_embedding_response = {"mock": "return"}
+    openai_mock.Embedding.create.return_value = expected_embedding_response
+
+    logger_mock = MagicMock()
+    monkeypatch.setattr(openai, "LOGGER", logger_mock)
+
+    mock_input = "mock_input"
+    mock_model = "mock-model"
+
+    expected_build_and_insert_response = {
+        "openai_response": expected_embedding_response,
+        "starpoint_response": None,
+    }
+
+    actual_build_and_insert_response = client.build_and_insert_embeddings(
+        model=mock_model, input_data=mock_input
+    )
+
+    assert actual_build_and_insert_response == expected_build_and_insert_response
+    collision_mock.assert_called_once()
+    starpoint_mock.column_insert.assert_not_called()
+    logger_mock.warning.assert_called_once_with(openai.NO_EMBEDDING_DATA_FOUND)
+
+
+@patch("starpoint.openai._utils._check_collection_identifier_collision")
+@patch("starpoint.openai.OpenAIClient._init_openai")
+@patch("starpoint.reader.Reader")
+@patch("starpoint.writer.Writer")
+def test_client_build_and_insert_embeddings_exception_during_write(
+    mock_writer: MagicMock,
+    mock_reader: MagicMock,
+    mock_init_openai: MagicMock,
+    collision_mock: MagicMock,
+    monkeypatch: MonkeyPatch,
+):
+    starpoint_mock = MagicMock()
+    client = openai.OpenAIClient(starpoint_mock)
+    openai_mock = MagicMock()
+    client.openai = openai_mock
+
+    expected_embedding_response = {
+        "data": [
+            {
+                "embedding": 0.77,
+                "index": 0,
+            }
+        ]
+    }
+    openai_mock.Embedding.create.return_value = expected_embedding_response
+
+    column_insert_error_message = "Test Exception"
+    starpoint_mock.column_insert.side_effect = RuntimeError(column_insert_error_message)
+
+    logger_mock = MagicMock()
+    monkeypatch.setattr(openai, "LOGGER", logger_mock)
+
+    mock_input = "mock_input"
+    mock_model = "mock-model"
+
+    expected_build_and_insert_response = {
+        "openai_response": expected_embedding_response,
+        "starpoint_response": {"error": column_insert_error_message},
+    }
+
+    build_and_insert_response = client.build_and_insert_embeddings(
+        model=mock_model, input_data=mock_input
+    )
+
+    assert build_and_insert_response == expected_build_and_insert_response
+    collision_mock.assert_called_once()
+    logger_mock.error.assert_called_once()
+
+
+@patch("starpoint.openai.OpenAIClient.build_and_insert_embeddings")
+@patch("starpoint.openai._utils._check_collection_identifier_collision")
+@patch("starpoint.openai.OpenAIClient._init_openai")
+@patch("starpoint.reader.Reader")
+@patch("starpoint.writer.Writer")
+def test_default_build_and_insert_embeddings(
+    mock_writer: MagicMock,
+    mock_reader: MagicMock,
+    mock_init_openai: MagicMock,
+    collision_mock: MagicMock,
+    build_and_insert_embeddings_mock: MagicMock,
+):
+    client = openai.OpenAIClient(MagicMock())
+    mock_input = "mock_input"
+
+    client.default_build_and_insert_embeddings(input_data=mock_input)
+
+    build_and_insert_embeddings_mock.assert_called_once_with(
+        model=openai.DEFAULT_OPENAI_MODEL,
+        input_data=mock_input,
+        collection_id=None,
+        collection_name=None,
+    )
